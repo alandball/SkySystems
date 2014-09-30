d680efd6f9b4c5748f9d590cd70c2850280577b9
Alan Ball <alandball@hotmail.com>
2014-09-30 22:16:05 +0200

    Connected to database

--- Data/Migrations/201409301957387_First.cs
+++ Data/Migrations/201409301957387_First.cs
@@ -3,7 +3,7 @@ namespace Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialCreate : DbMigration
+    public partial class First : DbMigration
     {
         public override void Up()
         {
