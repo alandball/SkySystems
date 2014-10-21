818d42b8974093b5e98971f001e67e6542474741
HenryLeRoux <henry.leroux@hotmail.com>
2014-10-21 17:40:44 +0200

    stock

---
+++ UI/Controllers/StockController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class StockController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Create()
+        {
+
+
+            throw new NotImplementedException();
+        }
+	}
+}
